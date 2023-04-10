f0059849d1c0891240877c3b7b1caa6014b67cc5
codedit334 <oussama7elabdioui@gmail.com>
2023-04-10 10:05:24 +0000

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+
+import React from "react"
+
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from 'react-router-dom';
-import './App.css';
+import './styles/App.css';
 import NavBar from './components/NavBar';
 import MyProfile from './components/MyProfile';
 
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 export default function MyProfile() {
   return (
     <div>My Profile</div>
