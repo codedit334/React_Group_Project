d0c9a82fb90d479c6e473a43d000f7937667a2ab
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-10 15:28:31 +0300

    Create Dragons component

---
+++ src/components/Dragons.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Dragons.css'
+const Dragons = ()=>{
+
+    return (
+        <>
+        <div className='main-container'>
+            <img src='#' alt='#'/>
+            <div className='details-container'>
+                <h3>header</h3>
+                <p>Some pargraph</p>
+                <button type='button'>Reserv Rocket</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Dragons;
