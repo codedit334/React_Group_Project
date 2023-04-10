b903403366bd9c604ac56dd0fc7d5a1a5dc04f45
shubham <shubham.bhoyar077@gmail.com>
2023-04-10 15:16:19 +0530

    Create navbar and folder structure

---
+++ src/components/MyProfile.js
@@ -0,0 +1,5 @@
+export default function MyProfile() {
+  return (
+    <div>My Profile</div>
+  );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <>
+      <nav>
+        <div>
+          <h1>Space Travelers&#39; Hub</h1>
+        </div>
+        <ul>
+          <NavLink>Rockets</NavLink>
+          <NavLink>Dragons</NavLink>
+          <NavLink>Mission</NavLink>
+          <NavLink to="my_profile" >My Profile</NavLink>
+        </ul>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
