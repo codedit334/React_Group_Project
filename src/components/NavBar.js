df274c50b93ad52a482ca576bec0497b0da024c5
shubham <shubham.bhoyar077@gmail.com>
2023-04-10 19:19:25 +0530

    Add basic styleing to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import logo from '../Assets/logo.png';
+import '../styles/navbar.css';
 
 export default function NavBar() {
   return (
     <>
-      <nav>
-        <div>
+      <nav className="navbar">
+        <div className="logo-name">
+          <img src={logo} alt="space logo" />
           <h1>Space Travelers&#39; Hub</h1>
         </div>
         <ul>
-          <NavLink to="/">Rockets</NavLink>
-          <NavLink>Dragons</NavLink>
-          <NavLink>Mission</NavLink>
-          <NavLink to="my_profile">My Profile</NavLink>
+          <li><NavLink to="/">Rockets</NavLink></li>
+          <li><NavLink>Dragons</NavLink></li>
+          <li><NavLink>Mission</NavLink></li>
+          <li><NavLink to="my_profile">My Profile</NavLink></li>
         </ul>
       </nav>
       <main>
