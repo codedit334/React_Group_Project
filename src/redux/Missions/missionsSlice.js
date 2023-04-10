8e51dec9f72509a8ed48ad7c91ad9f147dee1c56
codedit334 <oussama7elabdioui@gmail.com>
2023-04-10 20:45:56 +0000

    Refactor leave mission

--- src/redux/Missions/missionsSlice.js
+++ src/redux/Missions/missionsSlice.js
@@ -8,6 +8,21 @@ const initialState = {
   error: null,
 };
 
+export const fetchMissions = createAsyncThunk(
+  'missions/fetchMissions',
+  async (initialState, { dispatch }) => {
+    const response = await axios.get(url);
+    const newArr = await response.data.map((item) => ({
+      mission_id: item.mission_id,
+      mission_name: item.mission_name,
+      description: item.description,
+    }));
+    // eslint-disable-next-line no-use-before-define
+    dispatch(missionAdded(newArr));
+    // return response.data;
+  },
+);
+
 const missionsSlice = createSlice({
   name: 'missions',
   initialState,
@@ -18,71 +33,34 @@ const missionsSlice = createSlice({
     },
     reserveMission(state, action) {
       const id = action.payload;
-      let newArr = state.missions.find((item) => {
+      const newArr = state.missions.map((item) => {
         if (item.mission_id === id) {
-          // item.reserved = true;
-          console.log({ ...item, reserved: true });
           return { ...item, reserved: true };
         }
         return item;
       });
-      // state.missions = newArr;
-      // return [...state.missions, ...newArr];
-      // return { ...state, missions: [...state.missions, ...newArr] };
-      // console.log([newArr, ...state.missions]);
-      newArr = state.missions.concat(newArr);
-      console.log(newArr);
       return { ...state, missions: newArr };
     },
     leaveMission(state, action) {
       const id = action.payload;
-      let newArr = state.missions.map((item) => {
+      const newArr = state.missions.map((item) => {
         if (item.mission_id === id) {
-          // item.reserved = false;
-          return { ...item, reserved: true };
+          return { ...item, reserved: false };
         }
         return item;
       });
-      // state.missions = newArr;
-      // return [...state.missions, ...newArr];
-      // return { ...state, missions: [...state.missions, ...newArr] };
-      newArr = newArr.concat(state.missions);
       return { ...state, missions: newArr };
     },
   },
-  /* eslint-disable */
 
   extraReducers(builder) {
     builder
-      .addCase(fetchMissions.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(fetchMissions.fulfilled, (state, action) => {
-        state.status = "succeeded";
-      })
-      .addCase(fetchMissions.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      });
-    /* eslint-enable */
+      .addCase(fetchMissions.pending, (state) => ({ ...state, status: 'loading' }))
+      .addCase(fetchMissions.fulfilled, (state) => ({ ...state, status: 'succeeded' }))
+      .addCase(fetchMissions.rejected, (state, action) => ({ ...state, status: 'failed', error: action.error.message }));
   },
 });
 
 export const { missionAdded, reserveMission, leaveMission } = missionsSlice.actions;
-
-export const fetchMissions = createAsyncThunk(
-  'missions/fetchMissions',
-  async (initialState, { dispatch }) => {
-    // eslint-disable-line
-    const response = await axios.get(url);
-    const newArr = await response.data.map((item) => ({
-      mission_id: item.mission_id,
-      mission_name: item.mission_name,
-      description: item.description,
-    }));
-    dispatch(missionAdded(newArr));
-    // return response.data;
-  },
-);
 
 export default missionsSlice.reducer;
