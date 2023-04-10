092edf018118450617ab9132b74805a1c801ed9b
shubham <shubham.bhoyar077@gmail.com>
2023-04-10 18:49:34 +0530

    Add reserved badges #25 as per instruction

--- src/components/RocketLi.js
+++ src/components/RocketLi.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket, cancelReservation } from '../redux/rockets/rocketsSlice';
 
 export default function RocketLi({ rocket }) {
+  const dispatch = useDispatch();
   return (
     <li>
       <img src={rocket.flickr_image} alt={rocket.rocket_name} />
       <h2>{rocket.rocket_name}</h2>
+      {rocket.reserved && <div>Reserved</div>}
       <p>{rocket.description}</p>
-      <button type="button">Reserve Rocket</button>
+      {rocket.reserved && <button type="button" onClick={() => dispatch(cancelReservation(rocket.id))}>cancel Reservation</button>}
+      {!rocket.reserved && <button type="button" onClick={() => dispatch(reserveRocket(rocket.id))}>Reserve Rocket</button>}
     </li>
   );
 }
@@ -18,5 +23,6 @@ RocketLi.propTypes = {
     rocket_name: PropTypes.string,
     description: PropTypes.string,
     flickr_image: PropTypes.string,
+    reserved: PropTypes.bool,
   }).isRequired,
 };
