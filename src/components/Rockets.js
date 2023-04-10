f80db9a58707a3fd5e4cf8cc58a6d58285cbded5
shubham <shubham.bhoyar077@gmail.com>
2023-04-10 17:57:52 +0530

    Render rockets list #33

---
+++ src/components/Rockets.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from '../redux/rockets/rocketsSlice';
+import RocketLi from './RocketLi';
+
+export default function Rockets() {
+  const { rockets, isRocketLoading } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  if (isRocketLoading) {
+    return (<h2>Loading...</h2>);
+  }
+
+  return (
+    <ul>
+      {rockets.map((rocket) => <RocketLi key={rocket.id} rocket={rocket} />)}
+    </ul>
+  );
+}
