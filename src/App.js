a0a616810d3ddeb5621c742a669e6020f688bf57
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-10 15:28:09 +0300

    Import Dragons component and add Dragons route

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import {
 import './styles/App.css';
 import NavBar from './components/NavBar';
 import MyProfile from './components/MyProfile';
+import Dragons from "./components/Dragons";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<NavBar />}>
       <Route path="/my_profile" element={<MyProfile />} />
+      <Route path="/Dragons" element={<Dragons />} />
     </Route>,
   ),
 );
