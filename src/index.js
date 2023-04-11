fbf1e4560889446de5fc6ad073f7d7217dc8e81e
codedit334 <oussama7elabdioui@gmail.com>
2023-04-11 11:15:31 +0000

    Fix loading bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { getRockets } from './redux/rockets/rocketsSlice';
+import { fetchMissions } from './redux/Missions/missionsSlice';
+import { fetchDragons } from './redux/Dragons/dragonSlice';
 import store from './redux/store';
 import './styles/index.css';
 import App from './App';
+
+store.dispatch(getRockets())
+store.dispatch(fetchMissions())
+store.dispatch(fetchDragons())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
