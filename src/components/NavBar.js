4d60b5543c409739a13213a80e02d3c6aebdf204
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-11 08:31:56 +0300

    Fix linters & add alt for img tag

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,21 @@
-import React from "react"
+import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import logo from '../Assets/logo.png'
-import '../styles/NavBar.css'
+import logo from '../Assets/logo.png';
+import '../styles/NavBar.css';
 
 export default function NavBar() {
   return (
     <>
       <nav>
         <div className="logo">
-          <img src={logo}/>
+          <img src={logo} alt="space-logo" />
           <h1>Space Travelers&#39; Hub</h1>
         </div>
         <ul>
           <NavLink>Rockets</NavLink>
           <NavLink to="Dragons">Dragons</NavLink>
           <NavLink>Mission</NavLink>
-          <NavLink to="my_profile" >My Profile</NavLink>
+          <NavLink to="my_profile">My Profile</NavLink>
         </ul>
       </nav>
       <main>
